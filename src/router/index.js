91217d25b2eba3b02d7836f4c053e7296793c6cb
Roomiik <73971455+Roomiik@users.noreply.github.com>
2026-03-22 22:48:21 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HistoryView from '@/views/HistoryView.vue'
+import SqlQueryView from '@/views/SqlQueryView.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/history',
     name: 'history',
     component: HistoryView
+  },
+  {
+    path: '/admin/sql',
+    name: 'sql-query',
+    component: SqlQueryView
   }
   // інші маршрути, якщо потрібні
 ]

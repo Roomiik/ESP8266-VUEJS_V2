10a30daf6eab7dc3c6a7f53de1a469408f88de67
Roomiik <73971455+Roomiik@users.noreply.github.com>
2026-07-24 16:22:58 +0000

    Update file

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -38,7 +38,7 @@ const selectedSensor = ref('');
 const chartData = ref([]);
 const selectedSensorName = ref('');
 
-const API_URL = 'https://esp8266-serve-v2.onrender.com/api'; // замініть на вашу адресу
+const API_URL = 'https://esp8266-servev2-production.up.railway.app/api'; // замініть на вашу адресу
 
 // Завантажуємо список датчиків
 onMounted(async () => {
